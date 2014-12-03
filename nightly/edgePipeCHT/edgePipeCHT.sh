f5534c69a547543d335c7b8f25d30a4d114985e4
Stefan P. Domino <spdomin@sandia.gov>
2014-12-03 12:39:50 -0700

    more sophiticated error check
    * two examples, now consolidate common operations

--- nightly/edgePipeCHT/edgePipeCHT.sh
+++ nightly/edgePipeCHT/edgePipeCHT.sh
@@ -1,36 +1,72 @@
 #!/bin/bash
 CWD=$(pwd)
 tolerance=0.0000000000000001
+didSimulationDiffAnywhere=0
 if [ -f $CWD/PASS ]; then
-    PASS_STATUS=1
+    didSimulationDiffAnywhere=0
 else
     mpiexec --np 4 ../../naluX -i edgePipeCHT.i -o edgePipeCHT.log
-    grep "Mean System Norm:" edgePipeCHT.log  | awk '{print $4}' > edgePipeCHT.norm
 
-    read localNorm < "edgePipeCHT.norm"
-    read goldNorm < "edgePipeCHT.norm.gold"
+    grep "Mean System Norm:" edgePipeCHT.log  | awk '{print $4 " " $5 " " $6 }' > edgePipeCHT.norm
 
-    modLocalNorm=$(printf "%1.32f" $localNorm)
-    modGoldNorm=$(printf "%1.32f" $goldNorm)
+# check for files...
 
+# read in gold norm values
+    goldCount=1
+    goldFileContent=( `cat "edgePipeCHT.norm.gold"`)
+    for gfc in "${goldFileContent[@]}"
+    do
+	goldNorm[goldCount]=$gfc
+	((goldCount++))
+    done
+
+# read in local norm values
+    localCount=1
+    localFileContent=( `cat "edgePipeCHT.norm"`)
+    for lfc in "${localFileContent[@]}"
+    do
+	localNorm[localCount]=$lfc
+	((localCount++))
+    done
+
+    if [ $(echo " $goldCount - $localCount" | bc) -eq 0 ]; then
+
+# the lengths the same... proceed
+
+	for ((i=0;i<$goldCount;++i)); do
+	    modLocalNorm=$(printf "%1.32f" ${localNorm[i]})
+	    modGoldNorm=$(printf "%1.32f" ${goldNorm[i]})
 # compute the difference
-    diff=$(echo $modLocalNorm - $modGoldNorm | bc)
- 
+	    diff=$(echo $modLocalNorm - $modGoldNorm | bc)
+
 # make sure diff is positive.. abs anyone?
-    zero=0.0
-    minusOne=-1.0
-    if [ $(echo " $diff < $zero" | bc) -eq 1 ]; then
-	absDiff=$(echo $diff*$minusOne | bc)
-    else
-	absDiff=$diff
-    fi
+	    zero=0.0
+	    minusOne=-1.0
+	    if [ $(echo " $diff < $zero" | bc) -eq 1 ]; then
+		absDiff=$(echo $diff*$minusOne | bc)
+	    else
+		absDiff=$diff
+	    fi
 
 # test the difference
-    if [ $(echo " $absDiff < $tolerance" | bc) -eq 1 ]; then
-	PASS_STATUS=1
-	echo $diff > PASS
+	    if [ $(echo " $absDiff > $tolerance" | bc) -eq 1 ]; then
+		didSimulationDiffAnywhere=1
+	    fi
+	done
+
     else
-	PASS_STATUS=0
+# length was not the same; fail
+	didSimulationDiffAnywhere=1
     fi
+
 fi
+
+# write the file based on final status
+if [ "$didSimulationDiffAnywhere" -gt 0 ]; then
+    PASS_STATUS=0
+else
+    PASS_STATUS=1
+    echo $diff > PASS
+fi
+
 exit $PASS_STATUS
