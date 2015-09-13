78d9b7295bd881505fa1138d96693007905a48d4
Stefan P. Domino <spdomin@sandia.gov>
2015-09-12 18:06:56 -0600

    BEGIN Merge with higherOrderCVFEM
    New quad9 rtest
    
    * push to new higher order branch
    
    New gold for quad9HC
    
    New gold due to bug fix in isoPar for SCV ho elem
    
    New gold due to new shifted scs dual face locations
    
    Activate PNG equation; diffs accepted due to new EQS
    
    Add solver name for new dqdxCMM
    
    new gold due to proper calling sequence for PNG
    
    New hoVortex rtest
    
    New STV
    
    New gold due to BDF2 scalar bug
    
    * new HO helium test
    
    New gold due to CMM for source and mix frac
    
    END Merge with higherOrderCVFEM
    
    * modify hoHelium to use 3d hex27
    
    * steadyTV to output norms and exact fields

---
+++ nightly/quad9HC/quad9HC.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+. ../../pass_fail.sh
+
+CWD=$(pwd)
+didSimulationDiffAnywhere=0
+
+# determine tolerance
+testTol=0.0000001
+platform=`uname`
+if [ "$platform" == 'Linux' ]; then
+    testTol=0.0000000000000001
+fi
+
+# set the global diff
+GlobalMaxSolutionDiff=-1000000000.0
+
+if [ -f $CWD/PASS ]; then
+    # already ran this test
+    didSimulationDiffAnywhere=0
+else
+    mpiexec --np 2 ../../naluX -i quad9HC.i -o quad9HC.log
+    determine_pass_fail $testTol "quad9HC.log" "quad9HC.norm" "quad9HC.norm.gold"
+    didSimulationDiffAnywhere="$?"
+fi
+
+# write the file based on final status
+if [ "$didSimulationDiffAnywhere" -gt 0 ]; then
+    PASS_STATUS=0
+else
+    PASS_STATUS=1
+    echo $PASS_STATUS > PASS
+fi
+
+# report it; 30 spaces
+GlobalPerformanceTime=`grep "STKPERF: Total Time" quad9HC.log  | awk '{print $4}'`
+if [ $PASS_STATUS -ne 1 ]; then
+    echo -e "..quad9HC..................... FAILED":" " $GlobalPerformanceTime " s" " max diff: " $GlobalMaxSolutionDiff
+else
+    echo -e "..quad9HC..................... PASSED":" " $GlobalPerformanceTime " s"
+fi
+
+exit
